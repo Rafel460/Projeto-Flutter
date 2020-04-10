a91889e0f0876d159967eaee28b78b2a02d510c3
Rafel460 <rafaelmonte46@outlook.com>
2020-04-09 21:26:40 -0300

    Versao 09-04

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,33 @@
-
-
 import 'package:flutter/material.dart';
 void main(){
-  runApp(
-    MaterialApp(
+  runApp(ProgramaBrabor());
+}
+void alertar(){
+  print('Teste 1,2');
+}
+
+class ProgramaBrabor extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.brown,
-          title: Text('Olá Mundo!'),
+        backgroundColor: Colors.grey, 
+        title:Text('Fala nerds, tranqs?', style: TextStyle(fontFamily: 'Verdana'),),
+        centerTitle: true,
         ),
         body: Center(
-          child: Title(color: Colors.black, child: Text('Isso é um teste')),
-
-        )
-        
-      )
-  ),
-  );
+          child: Column( //-> Cria um child com uma coluna de elementos!
+            children: <Widget>[ //Atributo com um array de widgets
+            Text('Peeeter Aqui', style: TextStyle(fontSize: 40.0,)),
+            FloatingActionButton(child: Icon(Icons.arrow_forward)),
+            
+          ],)
+          //child: 
+  
+        ),
+      ),
+      
+    );
+  }
 }
